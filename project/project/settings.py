4d70059270c136c3e0d6c114d1bf6f36f2358352
ozz1webdev <ozz1webdev@gmail.com>
2024-06-04 06:32:16 +0200

    add readme images

--- project/project/settings.py
+++ project/project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1','debian','ubuntu']
+ALLOWED_HOSTS = ['patchwerk.ddns.net']
 
 
 # Application definition
