cfa8838070c670ec479a3685fd2c7f457d89256b
ozz1webdev <ozz1webdev@gmail.com>
2025-01-08 05:09:23 +0100

    relocate files

--- project/project/settings.py
+++ project/project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['patchwerk.ddns.net']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'webserver', 'patchwerk.ddns.net']
 
 
 # Application definition
